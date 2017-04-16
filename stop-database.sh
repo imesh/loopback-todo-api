ac8ca447d7f77d789410f2c41c70cf3576129717
Imesh Gunaratne <imesh.gunaratne@gmail.com>
2017-04-16 12:10:49 +0530

    Add stop-database.sh script

--- stop-database.sh
+++ stop-database.sh
@@ -3,9 +3,8 @@
 name="postgres-db"
 existing_container=$(docker ps -a | awk '{print $NF}' | grep "${name}")
 if [[ $existing_container = "$name" ]]; then
-    echo "A container with the name ${name} found, removing it..."
+    echo "Stopping container ${name}..."
     docker rm -f "${name}" > /dev/null 2>&1 || { echoError "Couldn't terminate container ${name}."; exit 1; }
+    echo "Container ${name} stopped"
 fi    
 
-echo "Starting ${name}..."
-docker run -p 5432:5432 --name ${name} -e POSTGRES_DB=todos -e POSTGRES_USER=postgres -e POSTGRES_PASSWORD=postgres -d postgres 
